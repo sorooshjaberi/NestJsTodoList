0dc3504ac70b329ab357ce97d49d74fbf7f16658
= <=>
2024-05-21 14:25:58 +0330

    added testing config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     abortOnError: false,
     cors: true,
   });
+
+  const options = new DocumentBuilder()
+    .setTitle('TodoList')
+    .setDescription('A todolist application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
--- backend/src/todos/todos.service.spec.ts
+++ backend/src/todos/todos.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodosService } from './todos.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Todo } from 'src/todos/entities/todos.entity';
+import { Repository } from 'typeorm';
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+const createRepository = <T = any>(): MockRepository<T> => ({
+  find: jest.fn(),
+  findOneBy: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+  preload: jest.fn(),
+  remove: jest.fn(),
+});
 
 describe('TodosService', () => {
   let service: TodosService;
+  let todoRepository: MockRepository<Todo>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodosService],
+      providers: [
+        TodosService,
+        {
+          provide: getRepositoryToken(Todo),
+          useValue: createRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<TodosService>(TodosService);
+
+    todoRepository = module.get<MockRepository<Todo>>(getRepositoryToken(Todo));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('should create the intended todo', async () => {
+      const todo = {
+        description: '',
+        done: true,
+        title: 'helo',
+        user: null,
+      };
+
+      todoRepository.create.mockReturnValue(todo);
+
+      const returnedTodo = await todoRepository.create();
+      expect(returnedTodo).toEqual(todo);
+    });
   });
 });
