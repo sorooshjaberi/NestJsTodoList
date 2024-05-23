6c5f72033837de96d8410102a543c5761cc096e3
= <=>
2024-05-24 02:56:50 +0330

    global stale time to 5 minutes

--- frontend/src/providers/query.provider.tsx
+++ frontend/src/providers/query.provider.tsx
@@ -2,7 +2,13 @@ import { FC, PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const QueryProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
